7e3b2c09d80843876211507bbab1e1bc7991ebe4
Jodie <jodielee062788@gmail.com>
2024-02-29 00:03:30 +1100

    fix code

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequielize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening on http://localhost:${PORT}'));
+    app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
