dfb7b96f4d51170462112fa5a9cec22da0d2bcab
Jodie <jodielee062788@gmail.com>
2024-03-03 23:47:33 +1100

    Set up post routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,87 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User, attributes: ['username'] }],
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        { model: User, attributes: ['username'] },
+        { model: Comment,
+          include: [{ model: User, attributes: ['username'] }],
+        },
+      ],
+    });
+    if (!postData) {
+      res.status(404).json({ message: "Post not found!" });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      userId: req.session.userId,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(req.body, {
+      where: { 
+        id: req.params.id },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "Post not found!" });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+      await Comment.destroy({
+        where: { 
+            postId: req.params.id },
+      });
+  
+      const postData = await Post.destroy({
+        where: { 
+            id: req.params.id },
+      });
+  
+      if (!postData) {
+        res.status(404).json({ message: "Post not found!" });
+        return;
+      }
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
