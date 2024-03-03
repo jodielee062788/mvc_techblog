d7e3ac9f587b96f5c41571b20be460df80116b92
Jodie <jodielee062788@gmail.com>
2024-03-04 10:49:29 +1100

    Modify helpers function and add js file for the functionality of the program

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,10 @@
 module.exports = {
+    // Format date as MM/DD/YYYY HH:MM:SS AM/PM
     format_date: (date) => {
-        return date.toLocaleDateString();
-    }
+        return date.toLocaleString();
+    },
+    // Capitalize the every string in the title
+    capitalize: (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    } 
 }
