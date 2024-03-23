f9e85ce10503e9a84f51741c75924571bdd4a4e2
Jodie <jodielee062788@gmail.com>
2024-03-24 10:27:34 +1100

    Modify delete route and made it fully functional

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,20 +1,21 @@
-const deletePostHandler = async (event) => {
-  if (event.target.classList.contains('deleteBtn')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/posts/${id}`, {
+const deletePost = async (id) => {
+  const response = await fetch(`/api/posts/${id}`, {
       method: 'DELETE',
     });
-    
+
     if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to delete project');
-    }
+          document.location.reload();
+        } else {
+          alert('Failed to delete project');
+        }
+      };
+
+const deletePostHandler = (event) => {
+  if (event.target.matches('.deleteBtn')) {
+    const id = event.target.getAttribute('data-id');
+    deletePost(id);
   }
 };
-
-document
-  .querySelector('.post-list')
-  .addEventListener('click', deletePostHandler);
+    
+document.addEventListener('click', deletePostHandler);
 
